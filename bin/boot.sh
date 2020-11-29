af7ad9448ecbd139cac9f7965406bcd6a0f5cd3e
Ingo Schwarz <ingo.schwarz23@gmail.com>
2020-11-29 18:19:48 +0100

    Rename application root directory

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export ROOT_DIR=$1
+export APP_ROOT_DIR=$1
 
 ruby $HOME/config/htpasswd.rb
 erb $HOME/config/nginx.conf.erb > $HOME/config/nginx.conf
@@ -10,5 +10,3 @@ touch $HOME/logs/nginx/access.log $HOME/logs/nginx/error.log
 
 (tail -f -n 0 $HOME/logs/nginx/*.log &)
 exec $HOME/vendor/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
-
-echo "Serving content of $ROOT_DIR"
